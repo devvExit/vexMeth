847c2e13793653ee444cb946a4258635cd91b1ff
Mr-Vexy <50603244+Mr-Vexy@users.noreply.github.com>
2020-04-10 16:11:05 +0200

    More Methods

--- methods/vexMeth.java
+++ methods/vexMeth.java
@@ -1,5 +1,8 @@
 package methods;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class vexMeth {
 
     public static String isThis420(String s, int i, double d) {
@@ -15,7 +18,7 @@ public class vexMeth {
       * trueIf | true | OR | true | OR | true |
       */
 
-    public int make(int a, int b, int c) {
+    public static int make(int a, int b, int c) {
         if (b == 0 && c <= a)
             return c;
         if (a == 0 && c % 5 == 0 && c <= b * 5)
@@ -37,9 +40,9 @@ public class vexMeth {
             e.printStackTrace();
         }
     }
-
+    // SplashScreen
     /*
-     * SplashScreen Welcome to vexMeth.java
+     * Welcome to vexMeth.java
      * 
      * What would you like to do?
      * 
@@ -47,6 +50,15 @@ public class vexMeth {
      * 
      * Note: Parameters are put in after selecting.
      */
+    // Make
+    /*
+     * Enter 3 integers xyz with no spaces: 509
+     * 
+     * It would take at least 4 small chocolate bars to make 9 units of chocolate.
+     * 
+     * Press enter to continue...
+     */
+
     public static void welcome(String[] args) {
         String iS = "";
         System.out.println("Welcome to vexMeth.java\n\nWhat would you like to do?\n\n"
@@ -68,7 +80,8 @@ public class vexMeth {
 
                 break;
             case "isThis420":
-
+                iS = input.next();
+                System.out.print(isThis420(iS, pInt(iS), doob(iS)));
                 break;
             default:
                 break;
@@ -76,7 +89,30 @@ public class vexMeth {
         input.close();
     }
 
+    private static int pInt(String n) {
+        int foo;
+        try {
+            foo = Integer.parseInt(n);
+            return foo;
+        } catch (NumberFormatException e) {
+            foo = 0;
+            return -1;
+        }
+    }
+
+    private static Double doob(String n) {
+        Double foo;
+        try {
+            foo = Double.parseDouble(n);
+            return foo;
+        } catch (NumberFormatException e) {
+            foo = 0.0;
+            return -1.0;
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.print(isThis420("", 420, 0));
+        cls(args);
+        welcome(args);
     }
 }
