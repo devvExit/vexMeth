453e4b0d6afb5abf22f0ce1ffdd638fb1cd81129
Mr-Vexy <50603244+Mr-Vexy@users.noreply.github.com>
2020-04-23 23:43:00 +0200

    Added docs to leapYear

--- vexmeth/leapYear.java
+++ vexmeth/leapYear.java
@@ -2,6 +2,7 @@ package vexmeth;
 
 public class leapYear {
 
+    // Result parts
     static String a = " is ";
     static String b = " is not ";
     static String c = " was ";
@@ -10,28 +11,41 @@ public class leapYear {
     static String f = " is not gonna be ";
     static String g = "a leap year";
 
-    static boolean l = false;
-    static int n = 2020;
-    static int i;
+    // Variables
+    static boolean l = false; // Will represent whether the current year is a leap year
+    static int n = 2020; // The current year
+    static int r = 17; // Range of years to check in each direction
+    static int i; // Iterator
 
     public static void main(String[] args) {
-        for (i = 1000; i <= n + 180; i++) {
-            l = (i % 400 == 0 || (i % 4 == 0 && i % 100 != 0));
-            System.out.println(isLeapYear(i));
+
+        // Clutter reductor v2.1
+        System.out.println();
+
+        // Main
+        for (i = n - r; i <= n + r; i++) { // Iterator is current year minus range, runs until last year in range
+            if (i == n) // *See below
+                System.out.println();
+            l = (i % 400 == 0 || (i % 4 == 0 && i % 100 != 0)); // Sets l to the result of the leap year logic
+            System.out.println(isLeapYear(i)); // Prints whether the current iteration was, is, or will be a leap year
+            if (i == n) // *Separates the current year from the rest
+                System.out.println();
         }
     }
 
     public static String isLeapYear(int year) {
 
+        // Checks for boolean l and whether the current iteration is before, during, or
+        // after the current year
         if (i == n && l)
             return year + a + g;
-        else if (i == n && !l)
+        if (i == n)
             return year + b + g;
-        else if (i < n && l)
+        if (i < n && l)
             return year + c + g;
-        else if (i < n && !l)
+        if (i < n)
             return year + d + g;
-        else if (i > n && l)
+        if (i > n && l)
             return year + e + g;
         else
             return year + f + g;
